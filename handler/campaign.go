0b9a94e3637585e2416eebc695d5a7ce288147b0
dzikrimardiansyah <dzikrimardi@gmail.com>
2022-07-25 12:13:30 +0700

    get campaigns endpoint (WIP)

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,34 @@
 package handler
+
+import (
+	"net/http"
+	"startup/campaign"
+	"startup/helper"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+//api1/v1/campaigns
+func (h *campaignHandler) GetCampaign(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaign, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaign)
+	c.JSON(http.StatusOK, response)
+
+}
