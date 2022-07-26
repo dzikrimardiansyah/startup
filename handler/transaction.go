57b7e0ca10c2208900d6f15edc72537f11a1f81f
dzikrimardiansyah <dzikrimardi@gmail.com>
2022-07-26 16:27:51 +0700

    campaign transaction endpoint

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,1 +1,43 @@
 package handler
+
+import (
+	"net/http"
+	"startup/helper"
+	"startup/transaction"
+	"startup/users"
+
+	"github.com/gin-gonic/gin"
+)
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransaction(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(users.User)
+
+	input.User = currentUser
+
+	transactions, err := h.service.GetTransactionByCampaignID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get campaign transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Get Transaction Success", http.StatusOK, "success", transaction.FormatCampaignTransactions(transactions))
+	c.JSON(http.StatusOK, response)
+}
--- transaction/input.go
+++ transaction/input.go
@@ -1,1 +1,8 @@
 package transaction
+
+import "startup/users"
+
+type GetCampaignTransactionsInput struct {
+	ID   int `uri:"id" binding:"required"`
+	User users.User
+}
